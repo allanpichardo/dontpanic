aafb1ae944de52deeb0bd1ca5fa2c4ab8a0f1ae8
Allan Pichardo <allan.pichardo@gmail.com>
2019-03-02 16:42:48 -0500

    merging with scaled vr

--- Assets/Scripts/Hoops/HoopsGame.cs
+++ Assets/Scripts/Hoops/HoopsGame.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HoopsGame : MonoBehaviour
 {
     public GameObject ballPrefab;
     public Transform ballSpawnPoint;
+    public Text scoreText;
 
     private int score = 0;
     
@@ -13,6 +15,16 @@ public class HoopsGame : MonoBehaviour
     {
         Physics.gravity = new Vector3(0, -100, 0);
         ShouldSpawnNewBall();
+    }
+
+    private void Start()
+    {
+        scoreText.text = "Score: 0";
+    }
+
+    private void Update()
+    {
+        scoreText.text = "Score: " + score;
     }
 
     public void ShouldSpawnNewBall()
@@ -23,6 +35,5 @@ public class HoopsGame : MonoBehaviour
     public void OnScoreMade()
     {
         score++;
-        ShouldSpawnNewBall();
     }
 }
