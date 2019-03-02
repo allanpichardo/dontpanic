bcdcbc359a12224387e457cbe43c59733d307fb7
Allan Pichardo <allan.pichardo@gmail.com>
2019-03-02 14:16:41 -0500

    hoops gravity set

--- Assets/Scripts/Hoops/HoopsGame.cs
+++ Assets/Scripts/Hoops/HoopsGame.cs
@@ -4,15 +4,8 @@ using UnityEngine;
 
 public class HoopsGame : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Physics.gravity = new Vector3(0, -100, 0);
     }
 }
