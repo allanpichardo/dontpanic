98e36585cb61829b3e26bc794b762d77eac70d29
Allan Pichardo <allan.pichardo@gmail.com>
2019-03-02 17:21:20 -0500

    grab pinch

--- Assets/Scripts/Hoops/BallGrabber.cs
+++ Assets/Scripts/Hoops/BallGrabber.cs
@@ -6,32 +6,24 @@ using Valve.VR.InteractionSystem;
 
 public class BallGrabber : MonoBehaviour
 {
-    public SteamVR_Action_Boolean TriggerClick;
+    public SteamVR_Action_Boolean grabPinch;
     public SteamVR_Input_Sources inputSource;
- 
-    private void Start() {} //Monobehaviours without a Start function cannot be disabled in Editor, just FYI
- 
-    private void OnEnable()
-    {
-        TriggerClick.AddOnStateDownListener(Press, inputSource);
-        TriggerClick.AddOnStateUpListener(Release, inputSource);
-    }
- 
-    private void OnDisable()
-    {
-        TriggerClick.RemoveOnStateDownListener(Press, inputSource);
-        TriggerClick.RemoveOnStateUpListener(Release, inputSource);
-    }
- 
-    private void Press(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
-    {
-        //put your stuff here
-        print("Trigger Down");
-    }
 
-    private void Release(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource)
+    private void Start()
     {
-        Debug.Log("Trigger Release");
+        grabPinch.AddOnChangeListener((fromAction, fromSource, newState) => 
+            {
+                if (newState)   // pressed
+                {
+                    Debug.Log("pressed");
+                }
+                else
+                {
+                    Debug.Log("released");
+                }
+            }
+            , inputSource);
     }
+ 
 
 }
