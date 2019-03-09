5e38f61cb0c416be281067782c629fc4c6b47d2a
Allan Pichardo <allan.pichardo@gmail.com>
2019-03-09 12:50:37 -0500

    added models and prefabs

--- Assets/Scripts/ZombieController.cs
+++ Assets/Scripts/ZombieController.cs
@@ -6,17 +6,30 @@ using UnityEngine.AI;
 public class ZombieController : MonoBehaviour
 {
     private NavMeshAgent navMeshAgent;
+    private Animator animator;
     public Transform player;
-    
+    private static readonly int _speed = Animator.StringToHash("speed");
+    private bool isDead = false;
+
     // Start is called before the first frame update
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        navMeshAgent.SetDestination(player.position);
+        if (!isDead)
+        {
+            navMeshAgent.SetDestination(player.position);
+            animator.SetFloat(_speed, navMeshAgent.velocity.magnitude);
+        }
+    }
+
+    public void SetDead(bool isDead)
+    {
+        this.isDead = isDead;
     }
 }
