b5bc5c5ba8694d3ccbcd99d0feb6d46c662aabf9
Allan Pichardo <allan.pichardo@gmail.com>
2019-03-09 16:12:31 -0500

    Trying to shoot and kill

--- Assets/CompanionController.cs
+++ Assets/CompanionController.cs
@@ -1,20 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class CompanionController : HandsEmpathyAgent
 {
+    public float distanceThreshold = 5.0f;
+    private NavMeshAgent navMeshAgent;
+    private Transform player;
+    private Animator animator;
+    private static readonly int _speed = Animator.StringToHash("speed");
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindGameObjectWithTag("player").transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Vector3.Distance(this.transform.position, player.position) > distanceThreshold)
+        {
+            navMeshAgent.SetDestination(player.position);
+            animator.SetFloat(_speed, navMeshAgent.velocity.magnitude);
+        }
     }
 
     public override void OnNewPrediction(Vector3 inference)
---
+++ Assets/Scripts/GunController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using HTC.UnityPlugin.Vive;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    private Animator animator;
+
+    private static readonly int _fire = Animator.StringToHash("fire");
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Awake()
+    {
+        ViveInput.AddPressDown(HandRole.RightHand, ControllerButton.Trigger, OnTrigger);
+    }
+
+    private void OnDestroy()
+    {
+        ViveInput.RemovePressDown(HandRole.RightHand, ControllerButton.Trigger, OnTrigger);
+    }
+
+    private void OnTrigger()
+    {
+        animator.SetTrigger(_fire);
+
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out var hit))
+        {
+            if (hit.collider.gameObject.layer == 17)
+            {
+                ZombieController zombieController = hit.collider.gameObject.GetComponent<ZombieController>();
+                zombieController.SetDead(true);
+            }
+        }
+    }
+}
