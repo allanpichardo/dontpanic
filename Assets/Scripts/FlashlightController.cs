eb08b00ae08c6cbae6132e8d073cea6a6c82dd28
Raphaëlle Giraud <ra_girau@live.concordia.ca>
2019-03-09 13:10:50 -0500

    flashlight script added

---
+++ Assets/Scripts/FlashlightController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FlashlightController : HandsEmpathyAgent
+{
+	private float emotion;
+	private float energy;
+	public light flashlight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public override void OnNewPrediction(Vector3 inference){
+    	this.emotion = inference.x;
+    	this.energy = inference.y;
+
+    	// emotion = ((emotion+1)*0.5);
+    	// energy = ((energy+1)*0.5);
+    	// float outputMultiplier = ((emotion+energy)*0.5);
+    	this.flashlight.spotAngle = 30+(energy*15);
+
+    }
+}
