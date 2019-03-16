f0ee4b6f1c5db20089ce9c39c29f7e69617ffaaf
Allan Pichardo <allan.pichardo@gmail.com>
2019-03-16 16:55:46 -0400

    bullets destroy themselves

---
+++ Assets/Scripts/BulletCasingController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletCasingController : MonoBehaviour
+{
+    private float time;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        time += Time.deltaTime;
+
+        if (time > 1.0f)
+        {
+            Destroy(this.gameObject);
+        }
+
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        Destroy(this.gameObject);
+    }
+}
